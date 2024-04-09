9b5cd8e89fdffaaa4996ca062558e49c7dd1b78f
nagopi <nagopi@iu.edu>
2024-04-09 16:04:07 -0400

    rabbitmq

--- src/main/java/edu/iu/nagopi/primesservice/controller/PrimesController.java
+++ src/main/java/edu/iu/nagopi/primesservice/controller/PrimesController.java
@@ -1,6 +1,9 @@
 package edu.iu.nagopi.primesservice.controller;
 
+import edu.iu.nagopi.primesservice.rabbitmq.MQSender;
 import edu.iu.nagopi.primesservice.service.IPrimesService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,12 +12,20 @@ import org.springframework.web.bind.annotation.*;
 public class PrimesController {
     IPrimesService primesService;
 
-    public PrimesController(IPrimesService primesService){
+    private final MQSender mqSender;
+
+    public PrimesController(IPrimesService primesService, MQSender mqSender){
         this.primesService = primesService;
+        this.mqSender = mqSender;
     }
 
     @GetMapping("/{n}")
     public boolean isPrime(@PathVariable int n){
-        return primesService.isPrime(n);
+        boolean result = primesService.isPrime(n);
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = ((Jwt) principal).getSubject();
+        System.out.println(username);
+        mqSender.sendMessage(username, n, result);
+        return result;
     }
 }
